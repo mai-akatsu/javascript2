5b8531d9370d4adafe9d154b53e6e460edf68675
EC2 Default User <ec2-user@ip-172-31-1-45.ap-northeast-1.compute.internal>
2023-01-14 06:57:19 +0000

    質問です

--- main.js
+++ main.js
@@ -21,13 +21,12 @@ let operator = document.getElementById("operator");
 
 operator.addEventListener('click', function(ope) {
     let ope_target = ope.innerHTML;
-    if(result.innerHTML.slick(-1) == operator){
-        let answer = result.innerHTML(0,-1);
-        result.innerHTML = answer + result.innerHTML;
+    
+    if(result.innerHTML.slice(-1) == ope_target){
+        let answer = result.innerHTML.slice(0,-1);
+        result.innerHTML = answer;
     }else{
         result.innerHTML += result.innerHTML;
     }
     
 }, false);
-
-
